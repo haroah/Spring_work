91a13e861530f0cac65a0614095a63715505fe42
Pharoah Jackson <pharoahjackson@pharoahs-air.bgsalesian.com>
2023-02-22 09:49:10 -0800

    first commit

--- Python/Rock_Paper_Scissors/rockpaperscissors.py
+++ Python/Rock_Paper_Scissors/rockpaperscissors.py
@@ -13,7 +13,7 @@ while True:
     if user_input not in options:
         continue
 
-    random_number = random.ranRdint(0, 2)
+    random_number = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
